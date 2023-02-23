64975133294f1b44434a8990a569d1bc0ad6b79c
Siem.p <64197688+Siem-P@users.noreply.github.com>
2023-02-23 23:23:39 +0100

    Added Comments

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -2,18 +2,21 @@ let squadMembers = null;
 
 const searchBar = document.getElementById('searchBar');
 searchBar.addEventListener('keyup', search)
+
+// Select all elements for checking search value with.
 squadMembers = document.querySelectorAll('.cards-container > .card')
-console.log(squadMembers)
+
+// Function for searching a member
 function search() {
-    
-    const searchValue = this.value.toLowerCase()
+    const searchValue = this.value.toLowerCase() // set the value to lower case
     if(this.value === '') {
       squadMembers.forEach(member => {
-        console.log(member)
-        member.hidden = false;
+        member.hidden = false; // each member is not hidden by default
       })
     } else {
       squadMembers.forEach(member => {
+        // if a value is equal to member, hide all the other members where this value is not equal to
+        // There is a toLowerCase built in to this one-liner
         member.hidden = !member.textContent.toLowerCase().includes(searchValue);
       })
     }
